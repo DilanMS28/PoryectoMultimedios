61debd2358f34cfa20171a144f3088e32a58bbc2
Dilan <dilantienda90@gmail.com>
2024-06-07 21:29:02 -0600

    agregar la pantalla de agendar

--- Navegacion.js
+++ Navegacion.js
@@ -14,6 +14,7 @@ import Home from "./navegacion/home";
 import Habitos from "./navegacion/habitos";
 import IMC from "./navegacion/imc";
 import Rutinas from "./navegacion/rutinas";
+import Agendar from "./navegacion/agendar";
 import MenuButton from "./components/menuButton"
 import Config from './navegacion/config'
 
@@ -54,7 +55,7 @@ function Drawers() {
       <DrawerNav.Screen name="habitos" component={Habitos} options={{drawerLabel:"Habitos Saludables"}} />
       <DrawerNav.Screen name="imc" component={IMC} options={{drawerLabel:"Indice Masa Corporal"}}/>
       <DrawerNav.Screen name="rutinas" component={Rutinas} options={{drawerLabel:"Rutina de Ejercicios"}}/>
-      <DrawerNav.Screen name="calendario" component={CrearCuenta} options={{drawerLabel:"Calendario"}} />
+      <DrawerNav.Screen name="calendario" component={Agendar} options={{drawerLabel:"Calendario"}} />
       <DrawerNav.Screen name="config" component={Config}  options={{drawerLabel:"Configuraciones"}}/>
 
     </DrawerNav.Navigator>
